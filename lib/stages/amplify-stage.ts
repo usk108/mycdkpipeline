0a94a38842d6e3f5a1541cc744de4dcb09b51a2e
usk108 <creaction.usk@gmail.com>
2021-12-08 22:23:51 +0900

    initial commit

--- lib/stages/amplify-stage.ts
+++ lib/stages/amplify-stage.ts
@@ -1,5 +1,5 @@
 import { CfnOutput, cfnTagToCloudFormation, Construct, Stage, StageProps } from '@aws-cdk/core';
-// IMPORT AMPLIFY EXPORTED BACKEND HERE
+import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
 import * as path from 'path'
 import * as cdk from '@aws-cdk/core'
 
@@ -7,7 +7,11 @@ export class AmplifyStage extends Stage {
   
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
-    
+
     // ADD AMPLIFY EXPORTED BACKEND STACK HERE
+    const amplifyStack = new AmplifyExportedBackend(this, "amplifyexportedbackend", {
+      path: path.resolve(__dirname, '..', 'amplify-export-mytodoapp'),
+      amplifyEnvironment: "dev"
+    })
   }
 }
